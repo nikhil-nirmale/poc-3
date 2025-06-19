99b7c2a3a64c39020e82e787bff50505d2bff4f1
nikhil-nirmale <nikhil.nirmale@5exceptions.com>
2025-06-19 17:10:09 +0530

    Update main.py

--- s-3/main.py
+++ s-3/main.py
@@ -1,5 +1,26 @@
+# main.py
+#  **Controller URL** | https://hopper202506172151188.saas.appdynamics.com
+#  **Account Name**   | hopper202506172151188  
+#  **Access Key**     | sg6s6tdflk8u  
+#  **Use SSL**        | True
+#  **Port**           | 443
+#  **Service Name**   | s-1
+
+import appdynamics.agent
 from flask import Flask
 import os
+
+# Initialize AppDynamics agent before app starts
+appdynamics.agent.start({
+    'controller': 'hopper202506172151188.saas.appdynamics.com',
+    'port': 443,
+    'ssl': True,
+    'account': 'hopper202506172151188',
+    'accesskey': 'sg6s6tdflk8u',
+    'application': 'HelloApp',
+    'tier': 's-1',
+    'node': 'Node-s-1'
+})
 
 app = Flask(__name__)
 
@@ -15,4 +36,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     print(f"🚀 Starting app on port {port}")
     app.run(host="0.0.0.0", port=port)
-
